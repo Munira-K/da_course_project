1cd8b8f32ad22352e4d81484095ced22154b3d7d
Munira-K <kurbanovamunira2003@gmail.com>
2024-11-28 22:50:02 +0500

    Update ddl.py

--- ddl.py
+++ ddl.py
@@ -13,18 +13,13 @@ def create_tables(database_path='my.db', tables_sql_path='queries/create_tables.
     
     with open(tables_sql_path, 'r', encoding='utf-8') as f:
         create_tables_sql = f.read()
-
     con.execute(create_tables_sql)
     con.close()
 
 
 
 def load_data(database_path='my.db', source_folder='source'):
-    """
-    Загружает данные из Excel-файлов в соответствующие таблицы базы данных.
-    """
     con = duckdb.connect(database=database_path)
-
     data_files = {
         'customers': 'customers',
         'services': 'services',
@@ -37,9 +32,7 @@ def load_data(database_path='my.db', source_folder='source'):
         file_path = os.path.join(source_folder, 'telecom_users.xlsx')
         if os.path.exists(file_path):
             df = pd.read_excel(file_path, sheet_name=sheet_name)
-
             con.execute(f"DELETE FROM {table_name};")
-
             try:
                 con.execute(f"INSERT INTO {table_name} SELECT * FROM df;")
                 print(f"Данные загружены в таблицу '{table_name}' из листа '{sheet_name}'.")
@@ -49,18 +42,13 @@ def load_data(database_path='my.db', source_folder='source'):
                 return
         else:
             print(f"Файл '{file_path}' не найден.")
-
     con.close()
 
 
 def create_views(database_path='my.db', views_sql_path='queries/create_views.sql'):
-    """
-    Создаёт представления (вьюшки) в базе данных DuckDB, выполняя SQL-скрипт.
-    """
     con = duckdb.connect(database=database_path)
     with open(views_sql_path, 'r') as f:
         create_views_sql = f.read()
-
     con.execute(create_views_sql)
     con.close()
 
