95c9a8cead6a18b82a0721c7dc25d05b52119f21
Munira-K <kurbanovamunira2003@gmail.com>
2024-11-30 09:13:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 # import plotly.express as px
 # from etl import fetch_data_from_view
 
-
+-- Создает главную страницу с разделениями по Таблицам и Кратким анализом
 pages = [
     st.Page('tables.py', title = 'Таблицы'),
     st.Page('dashboards.py', title = 'Краткий анализ')
