73e014a891e97a24142791744412644b46743d28
Munira-K <kurbanovamunira2003@gmail.com>
2024-11-30 09:06:24 +0500

    Update etl (+descriptions).py

--- etl.py
+++ etl.py
@@ -2,7 +2,12 @@ import duckdb
 import pandas as pd
 
 def fetch_data_from_view(view_name, database_path='my.db'):
-
+    """
+    Извлекает данные из вьюшки с бд и возвращает их в виде DataFrame.Работает следующим образом:
+        - Создаёт соединение с базой данных.
+        - Извлекает все данных из указанной вьюшки.
+        - Закрывает соединение и возвращает результат в виде DataFrame.
+    """
     query = f"SELECT * FROM {view_name};"
     con = duckdb.connect(database=database_path)
     df = con.execute(query).fetchdf()
